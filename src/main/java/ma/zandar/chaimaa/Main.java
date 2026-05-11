0be551258610619571e61b9587621ad73a190d89
chaimaa <zandar.chaimaa@gmail.com>
2026-05-11 10:55:52 +0200

    Ajout de tous le projet

--- src/main/java/ma/zandar/chaimaa/Main.java
+++ src/main/java/ma/zandar/chaimaa/Main.java
@@ -1,4 +1,4 @@
-package ma.enset;
+package ma.zandar.chaimaa;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
---
+++ src/main/java/ma/zandar/chaimaa/examjee/entities/Client.java
@@ -0,0 +1,4 @@
+package ma.zandar.chaimaa.examjee.entities;
+
+public class Client {
+}
